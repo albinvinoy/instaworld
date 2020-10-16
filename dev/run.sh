58b2162384e703f26d6b9404f4a073f3e209ee9f
Albin Vioy <albinvioy@Albins-MacBook-Pro.local>
2020-10-15 19:51:11 -0700

    added start script

---
+++ dev/run.sh
@@ -0,0 +1,7 @@
+./gradlew clean
+./gradlew goJF
+./gradlew build
+./gradlw bootjar
+pwd
+cd build/libs
+java -jar *.jar
--- src/main/java/com/social/instagram/InstagramApplication.java
+++ src/main/java/com/social/instagram/InstagramApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InstagramApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InstagramApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(InstagramApplication.class, args);
+  }
 }
